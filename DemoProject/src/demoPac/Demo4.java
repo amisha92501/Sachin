0181aa53ce496b5c93b79799ef218945a9d8b40e
Raju Kumar <kumarraju92501@gmail.com>
2023-10-19 12:36:02 +0530

    Modifies

--- DemoProject/src/demoPac/Demo4.java
+++ DemoProject/src/demoPac/Demo4.java
@@ -4,7 +4,10 @@ public class Demo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Well done");
+		System.out.println("Well done");
+		System.out.println("Well done");
+		System.out.println("Well done");
 	}
 
 }
